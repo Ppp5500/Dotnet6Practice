335fb4ef5acfb48f853b9b4bcc46e8d34a06516d
woosub <98096591+Ppp5500@users.noreply.github.com>
2023-01-25 22:34:13 +0900

    Init Repository

---
+++ Chaper06/PacktLibrary/Person.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+public class Person : Object
+{
+    // fields
+    public string? Name; // ? allows null
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($" {Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    // static method to "multiply"
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+
+    // instance method to "multiply"
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+}
---
+++ Chaper06/PeopleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using Packt.Shared;
+
+Person mike = new()
+{
+    Name = "Mike",
+    DateOfBirth = DateTime.Now,
+};
+
+Person mary = new() { Name = "mary" };
+Person jill = new() { Name = "Jill" };
+
+// call instance methos
+Person baby1 = mary.ProcreateWith(mike);
+baby1.Name = "Gary";
+
+// call static methos
+Person baby2 = Person.Procreate(mary, jill);
+WriteLine($"{mike.Name} has {mary.Children.Count} children");
+WriteLine($"{mary.Name} has {mary.Children.Count} children");
+WriteLine($"{jill.Name} has {mary.Children.Count} children");
+WriteLine(
+    format: "{0}'s first child is named \"{1}\".",
+    arg0: mary.Name,
+    arg1: mary.Children[0].Name);
