bfc4dd981e2b650b2010ac82d5b2186b84a5d70f
woosub <98096591+Ppp5500@users.noreply.github.com>
2023-01-29 22:57:10 +0900

    Structure Type

---
+++ Chaper06/PacktLibrary/DvdPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Packt.Shared;
+public class DvdPlayer : IPlayable
+{
+    public void Pause()
+    {
+        WriteLine("DvdPlayer is pausing");
+    }
+
+    public void Play()
+    {
+        WriteLine("DvdPlayer is playing");
+    }
+}
---
+++ Chaper06/PacktLibrary/IPlayable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Packt.Shared;
+public interface IPlayable
+{
+    void Play();
+    void Pause();
+    void Stop()
+    {
+        WriteLine("Default implementation of Stop");
+    }
+}
