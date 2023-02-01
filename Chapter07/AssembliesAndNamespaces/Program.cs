a9e1f7a1af0d33b0bdb8e2a5725400efeeb9e7fd
Ppp5500 <pinkaulait@gmail.com>
2023-02-01 18:12:40 +0900

    Update

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,1 +1,11 @@
-﻿Xdocument doc = new();
+﻿using System.Xml.Linq;
+using static System.Console;
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
