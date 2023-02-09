15bad001b8d31a28043bc1fc959b81dfb85ef9c4
woosub <98096591+Ppp5500@users.noreply.github.com>
2023-02-09 22:46:21 +0900

    update

--- Chapter09/WorkingWithJson/Program.cs
+++ Chapter09/WorkingWithJson/Program.cs
@@ -1,2 +1,10 @@
 ﻿using System.Text.Json; // JsonSerializer
 using System.Text.Json.Serialization; // [JsonInclude]
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+public class Book
+{
+    // constructor to set non-nullable property
+}
