05290eb509574992c197210753f4df7ac8805222
Ppp5500 <pinkaulait@gmail.com>
2023-03-09 17:44:47 +0900

    Update

---
+++ Chapter12/AsyncEnumerable/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+await foreach(int number in GetNumbersAsync())
+{
+    WriteLine($"Number: {number}");
+}
+async static IAsyncEnumerable<int> GetNumbersAsync()
+{
+    Random r = new();
+    // sinulate work
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 1001);
+}
